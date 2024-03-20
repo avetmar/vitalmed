a0ecb6aa35b82d29462bbd6e52fa125c23518735
Avet <avet.martirosyan.it@gmail.com>
2024-03-20 19:13:49 +0400

    adaptive 1199px coplete index

--- src/js/script.js
+++ src/js/script.js
@@ -1,55 +1,59 @@
 $(document).ready(function(){
-  $('.carousel_inner').slick({
-      infinite: true,
-      dots: true,
-      speed: 1200,
-      slidesToShow: 3,
-      slidesToScroll: 1,
-      autoplay: true,
-      autoplaySpeed: 2000,
-      arrows: false,
-      pauseOnFocus: false
-  });
+  try {
+    $('.carousel_inner').slick({
+        infinite: true,
+        dots: true,
+        speed: 1200,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        arrows: false,
+        pauseOnFocus: false
+    });
 
-  $('.carousel_advantages').slick({
-      infinite: true,
-      speed: 1200,
-      slidesToShow: 1,
-      slidesToScroll: 1,
-      autoplay: true,
-      autoplaySpeed: 2000,
-      arrows: false,
-      pauseOnFocus: false,
-      fade: true,
-      cssEase: 'linear'
-  });
+    $('.carousel_advantages').slick({
+        infinite: true,
+        speed: 1200,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        arrows: false,
+        pauseOnFocus: false,
+        fade: true,
+        cssEase: 'linear'
+    });
 
-  $('.carousel_results').slick({
-      infinite: true,
-      dots: true,
-      speed: 1200,
-      slidesToShow: 3,
-      slidesToScroll: 1,
-      autoplay: true,
-      autoplaySpeed: 2000,
-      arrows: false,
-      pauseOnFocus: false
-  });
+    $('.carousel_results').slick({
+        infinite: true,
+        dots: true,
+        speed: 1200,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        arrows: false,
+        pauseOnFocus: false
+    });
 
-  $('#consult form').validate({
-    rules: {
-      name: "required",
-      phone: "required",
-      check: "required"
-    },
-    messages: {
-      name: "Пожалуйста введите свое имя",
-      phone: "Пожалуйста введите свой номер",
-      check: "*"
-    }
-  });
-
-  $('input[name=phone]').mask("+374 (99) 99-99-99");
+    $('#consult form').validate({
+      rules: {
+        name: "required",
+        phone: "required",
+        check: "required"
+      },
+      messages: {
+        name: "Пожалуйста введите свое имя",
+        phone: "Пожалуйста введите свой номер",
+        check: "*"
+      }
+    });
+  } catch (e) {}
+  
+  try {
+    $('input[name=phone]').mask("+374 (99) 99-99-99");
+  } catch (e) {}
 
   $('form').submit(function(e) {
     e.preventDefault();
@@ -88,12 +92,14 @@ $(document).ready(function(){
     }
   });
 
-  $("a[href^='#']").click(function(){
-    const _href = $(this).attr("href");
-    $("html, body").animate({scrollTop: $(_href).offset().top+-140+"px"});
-    return false;
-  });
-  
+  try {
+    $("a[href^='#']").click(function(){
+      const _href = $(this).attr("href");
+      $("html, body").animate({scrollTop: $(_href).offset().top+-135+"px"});
+      return false;
+    });
+  } catch (e) {}
+    
   new WOW().init();
   
 });
