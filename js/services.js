411b765fd94ea3f5444a30950e7dd2894c17c898
Avet home <avet.maeritosyan.it@gmail.com>
2024-07-29 22:15:38 +0400

    implant page update, problem with bazal section

--- js/services.js
+++ js/services.js
@@ -1,13 +1,25 @@
-document.querySelector(".buttonMoreExpress").addEventListener("click", function () {
+document.querySelector(".buttonExpress").addEventListener("click", function () {
   const height = document.querySelector(".ekspress_implant");
-  const buttonText = document.querySelector(".buttonMoreExpress").textContent.trim();
+  const buttonText = this.textContent.trim();
 
   if (buttonText === "Узнать больше") {
-    height.style.maxHeight = "inherit";
-    document.querySelector(".buttonMoreExpress").textContent = "Скрыть";
+    height.style.maxHeight = height.scrollHeight + "px";
+    this.textContent = "Скрыть";
   } else {
     height.style.maxHeight = "480px";
-    document.querySelector(".buttonMoreExpress").textContent = "Узнать больше";
+    this.textContent = "Узнать больше";
   }
-  document.querySelector(".ekspress_implant").scrollIntoView({ behavior: "smooth" });
+});
+
+document.querySelector(".buttonZygoma").addEventListener("click", function () {
+  const height = document.querySelector(".zygoma");
+  const buttonText = this.textContent.trim();
+
+  if (buttonText === "Узнать больше") {
+    height.style.maxHeight = height.scrollHeight + "px";
+    this.textContent = "Скрыть";
+  } else {
+    height.style.maxHeight = "480px";
+    this.textContent = "Узнать больше";
+  }
 });
