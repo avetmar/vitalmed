6083e0f28dd8a4a1ffcb904c896e24119fe8cf81
Avet <avet.martirosyan.it@gmail.com>
2024-07-30 15:05:40 +0400

    update js

--- js/services.js
+++ js/services.js
@@ -1,25 +1,31 @@
-document.querySelector(".buttonExpress").addEventListener("click", function () {
-  const height = document.querySelector(".ekspress_implant");
-  const buttonText = this.textContent.trim();
+document.querySelectorAll(".buttonMore").forEach((button) => {
+  button.addEventListener("click", function () {
+    let height;
+    let maxHeight;
+    if (this.classList.contains("buttonExpress")) {
+      height = document.querySelector(".ekspress_implant");
+      maxHeight = "480px";
+    } else if (this.classList.contains("buttonZygoma")) {
+      height = document.querySelector(".zygoma");
+      maxHeight = "480px";
+    } else if (this.classList.contains("buttonBazal")) {
+      height = document.querySelector(".bazal");
+      maxHeight = "580px";
+    } else if (this.classList.contains("buttonFullImplant")) {
+      height = document.querySelector(".fullImplant");
+      maxHeight = "580px";
+    }
 
-  if (buttonText === "Узнать больше") {
-    height.style.maxHeight = height.scrollHeight + "px";
-    this.textContent = "Скрыть";
-  } else {
-    height.style.maxHeight = "480px";
-    this.textContent = "Узнать больше";
-  }
-});
-
-document.querySelector(".buttonZygoma").addEventListener("click", function () {
-  const height = document.querySelector(".zygoma");
-  const buttonText = this.textContent.trim();
-
-  if (buttonText === "Узнать больше") {
-    height.style.maxHeight = height.scrollHeight + "px";
-    this.textContent = "Скрыть";
-  } else {
-    height.style.maxHeight = "480px";
-    this.textContent = "Узнать больше";
-  }
+    const buttonText = this.textContent.trim();
+    if (buttonText === "Узнать больше") {
+      height.style.maxHeight = height.scrollHeight + "px";
+      this.textContent = "Скрыть";
+    } else {
+      height.style.maxHeight = maxHeight;
+      // Get the element's position and adjust by 200px
+      const elementPosition = height.getBoundingClientRect().top + window.scrollY - 100;
+      window.scrollTo({ top: elementPosition, behavior: "smooth" });
+      this.textContent = "Узнать больше";
+    }
+  });
 });
